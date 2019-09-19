4ecabc7d54a957aa384b7091c0007e266af2b792
Alan Krajina <alankrajina88@gmail.com>
2019-09-19 09:57:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ class Driver {
   passengers() {
     return store.trips.filter(
         function(trip) {
-  //        if (trip.driverId === this.id && trip.passenger === trip.passengerId){
-            return trip.passenger;
+          if (trip.driverId === trip.passengerId){
+            return trip.passenger;}
         }.bind(this)
     );
   }
